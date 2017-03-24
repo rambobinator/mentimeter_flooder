29ca732a2fd6b61d5e504faffae4ecd0e42e070c
rmasse <roman.masse@gmail.com>
2017-03-24 23:33:20 +0100

    Handle quiz questions by always returning the correct answer

--- flooder/menti_valid_choice.py
+++ flooder/menti_valid_choice.py
@@ -1,9 +1,22 @@
 from .base_menti_question import BaseMentiQuestion
 
 class MentiValidChoice(BaseMentiQuestion):
-    # choices
-    # property len(choices)
-    # correct choice
-    pass
+
+	needed_attr = ["choices", "countdown_to_vote"]
+
+	def __init__(self, params=None):
+		super().__init__(params)
+
+	def get_correct_answer(self):
+		for choice in self.choices:
+			if choice.get("correct_answer"):
+				return choice
+		return None
+
+	def flood(self):
+		correct_choice = self.get_correct_answer()
+		if correct_choice is not None:
+			return correct_choice.get("id")
+		return None
 
 __all__ = ["MentiValidChoice"]
