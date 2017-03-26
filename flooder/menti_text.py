9c39ba6725335110bf6cc90ea2117eae5cf2d014
rmasse <roman.masse@gmail.com>
2017-03-26 22:23:48 +0200

    Handle open text question by returning random quote

--- flooder/menti_text.py
+++ flooder/menti_text.py
@@ -1,7 +1,26 @@
+from json import loads
+from random import choice
+
 from .base_menti_question import BaseMentiQuestion
 
-class MentiText(BaseMentiQuestion):
-    # MAX 140 chars
-    pass
+QUOTES_PATH = 'quotes.json'
 
-__all__ = ["MentiText"]
+def get_quotes(filename=QUOTES_PATH):
+    with open(filename) as f:
+        return f.read()
+    return ''
+
+class MentiText(BaseMentiQuestion):
+
+    max_text_len = 140
+    needed_attr = []
+
+    def __init__(self, params=None):
+        super().__init__(params)
+        self.dictionary = loads(get_quotes())
+
+    def flood(self):
+        quote = choice(self.dictionary)
+        response = "{}: \"{}\"".format(quote.get("name", ''),
+                                        quote.get("quote", ''))
+        return response[:self.max_text_len]
