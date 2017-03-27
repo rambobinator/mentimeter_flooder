d83862309ace9cfd419c5dd05cac1a077aa015cc
rmasse <roman.masse@gmail.com>
2017-03-27 23:20:48 +0200

    Handle choices based questions by randomly return a response

--- flooder/menti_choice.py
+++ flooder/menti_choice.py
@@ -1,8 +1,15 @@
+from random import choice
+
 from .base_menti_question import BaseMentiQuestion
 
 class MentiChoice(BaseMentiQuestion):
-    # choices
-    # property len(choices)
-    pass
+
+    needed_attr = ["choices"]
+
+    def __init__(self, params=None):
+        super().__init__(params)
+
+    def flood(self):
+        return choice(self.choices).get("id")
 
 __all__ = ["MentiChoice"]
